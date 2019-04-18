0a3b4c5ca5335c3b46e88ccbfd691ae0651047c9
lingxiao <me@raymondlx.com>
2019-04-17 21:11:24 -0500

    image url finished

--- routes/image.js
+++ routes/image.js
@@ -40,5 +40,36 @@ imageRoute.post((req,res)=>{
 
     });
 
+// delete image in Pet Schema
+imageRoute.delete((req,res)=>{
+    let url = req.body.url;
+    let pet_id = req.body.pet_id;
+    if (!pet_id){
+        res.status(statusCode.NOT_FOUND).send({
+            message:"Pet Id not valid",
+            data:[]
+        })
+    }
+    else{
+        petMongoose.updateOne(
+            {'_id':pet_id},
+            {'$pull':{imageURLs:{'$in':url}}},
+            (err,pet_res)=>{
+                if (err){
+                    res.status(statusCode.SERVER_ERR).send({
+                        message:"Server Error",
+                        data:[]
+                    });
+                }
+                else{
+                    res.status(statusCode.OK).send({
+                        message:"Image url deleted",
+                        data: []
+                    })
+                }
+            });
+    }
+
+});
 
 module.exports = router;

e98a17618529b88775c4a33fecbfc0bda814142a
Kexiang <codywang1029@outlook.com>
2019-04-22 13:39:33 -0500

    set password to be not unique

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     location: {type: String, default: 'unknown location'},
     favoritedPets: [String],

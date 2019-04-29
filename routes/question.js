9ef13ec8a1b9cf905b0a1a624b8c770b5a428494
Yilin Ma <yilinma3@illinois.edu>
2019-04-28 20:49:11 -0500

    added auth for post questions

--- routes/question.js
+++ routes/question.js
@@ -4,56 +4,69 @@ const petMongoose = require('../models/pets.js');
 const userMongoose = require('../models/user.js');
 const questionMongoose = require('../models/question.js');
 
-
 const util = require('../util/util.js');
 const statusCode = util.statusCode;
+
+const passport = require('passport');
 
 const questionRoute = router.route('/');
 
 /**
  * Create a question under a pet. Update the pet's question array and user's question array accordingly.
  */
-questionRoute.post((req,res)=>{
+questionRoute.post(async (req,res)=>{
     const question = new questionMongoose(req.body);
-    const author = req.body.author;
     const pet = req.body.pet;
-
-    question.save((err,dbRes)=>{
-        if (err)
-            return res.status(statusCode.SERVER_ERR).send({
-                message: "Server error!",
-                data: {}
-            });
-        else{
-            // after saved, update User Schema
-            userMongoose.updateOne(
-                {'_id':author},
-                {'$addToSet':{questionsCreated:dbRes._id}}, (err,userRes)=>{
-                    if (err){
+    try {
+        await passport.authenticate('jwt', {}, (ret) => {
+            if (ret !== null) {
+                // user is logged in
+                let author = ret._id;
+                question.save((err,dbRes)=>{
+                    if (err)
                         return res.status(statusCode.SERVER_ERR).send({
                             message: "Server error!",
                             data: {}
                         });
-                    }
-                });
+                    else{
+                        // after saved, update User Schema
+                        userMongoose.updateOne(
+                          {'_id':author},
+                          {'$addToSet':{questionsCreated:dbRes._id}}, (err,userRes)=>{
+                              if (err){
+                                  return res.status(statusCode.SERVER_ERR).send({
+                                      message: "Server error!",
+                                      data: {}
+                                  });
+                              }
+                          });
 
-            petMongoose.updateOne(
-                {'_id':pet},
-                {'$addToSet':{questions:dbRes._id}}, (err,petRes)=>{
-                    if (err){
-                        return res.status(statusCode.SERVER_ERR).send({
-                            message: "Server error!",
-                            data: {}
+                        petMongoose.updateOne(
+                          {'_id':pet},
+                          {'$addToSet':{questions:dbRes._id}}, (err,petRes)=>{
+                              if (err){
+                                  return res.status(statusCode.SERVER_ERR).send({
+                                      message: "Server error!",
+                                      data: {}
+                                  });
+                              }
+                          });
+
+                        return res.status(statusCode.OK).send({
+                            message: "OK",
+                            data: dbRes
                         });
                     }
                 });
-
-            return res.status(statusCode.OK).send({
-                message: "OK",
-                data: dbRes
-            });
-        }
-    })
+            } else {
+                return res.status(401).send({
+                    message: "Unauthorized"
+                });
+            }
+        })(req, res);
+    } catch(err) {
+        return res.status(500).send({message: err});
+    }
 });
 
 const questionIDRoute = router.route('/:id');

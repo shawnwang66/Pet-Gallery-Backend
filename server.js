b4786f28fb6be46916ec45afcc68386683ca48a7
Yilin Ma <yilinma3@illinois.edu>
2019-05-02 14:44:07 -0500

    fix CORS

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header('Access-Control-Allow-Credential', true);
     next();
 };
 app.use(allowCrossDomain);
